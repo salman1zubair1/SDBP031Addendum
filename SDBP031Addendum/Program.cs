f18b7005b2add1e49ccb16eb03177673201f2aed
salma <salman1zubair@gmail.com>
2026-09-25 12:08:26 -0400

    program updates

--- SDBP031Addendum/Program.cs
+++ SDBP031Addendum/Program.cs
@@ -11,24 +11,71 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+// --- START OF AUTO-MIGRATION CODE TO CREATE TABLES ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate(); // This automatically executes migrations and creates tables
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while creating the database tables.");
+    }
+}
+// --- END OF AUTO-MIGRATION CODE ---
+
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+
+//using Microsoft.EntityFrameworkCore;
+//using SDBP031Addendum.Data;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+////builder.Services.AddOpenApi();
+
+//var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    //app.MapOpenApi();
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+////app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 
 
 //var builder = WebApplication.CreateBuilder(args);
